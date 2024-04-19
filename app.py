1e61aabeb5d14e8ed4c9f050188c2253e20055d9
ADITYA NEVE <64825422+Aditya21neve@users.noreply.github.com>
2024-04-19 16:11:16 +0530

    implement flask serrver

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ def index():
 
 @app.route('/static/css/<path:path>')
 def send_css(path):
-    return send_from_directory('static/css/style.css', path)
+    return send_from_directory('static/css', path)
 
 @app.route('/static/js/<path:path>')
 def send_js(path):
-    return send_from_directory('static/js/index.js', path)
+    return send_from_directory('static/js', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,63 @@
+
+  function filterBoxes() {
+    // Get all checkboxes
+    var checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
+
+    // Loop through checkboxes
+    checkboxes.forEach(function (checkbox) {
+      // Get the target box corresponding to the checkbox
+      var targetBoxId = checkbox.getAttribute('data-target');
+      var targetBox = document.getElementById(targetBoxId);
+
+      // Check if the checkbox is checked
+      if (checkbox.checked) {
+        // Display the corresponding box
+        targetBox.style.display = 'block';
+      } else {
+        // Hide the corresponding box
+        targetBox.style.display = 'none';
+      }
+    });
+  }
+
+  // Attach event listener to checkboxes
+  var checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', filterBoxes);
+  });
+
+
+
+// // Function to extract video ID from YouTube URL
+// // Function to extract video ID from YouTube URL
+// function extractVideoId(url) {
+//     var videoId = '';
+//     var match = url.match(/(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\r\n]{11})/);
+//     if (match) {
+//         videoId = match[1];
+//     } else {
+//         console.log('No video ID found in URL:', url);
+//     }
+//     return videoId;
+// }
+
+// // Function to handle form submission
+// function handleSubmit(event) {
+//     event.preventDefault(); // Prevent default form submission behavior
+    
+//     // Get the input URL from the form
+//     var inputUrl = document.getElementById('urlbox').value.trim();
+    
+//     // Extract video ID from the input URL
+//     var videoId = extractVideoId(inputUrl);
+
+//     // Log the extracted video ID to the console for debugging
+//     console.log('Extracted Video ID:', videoId);
+
+//     // Display an alert with the extracted video ID
+//     alert('Video ID: ' + videoId);
+// }
+
+// // Attach event listener to form submission
+// var form = document.querySelector('.search-container form');
+// form.addEventListener('submit', handleSubmit);
